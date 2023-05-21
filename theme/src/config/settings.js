991e2be151d3394eeacc5a85e1fedabef01c962b
SShashi <shashi.salavath@gmail.com>
2023-05-20 20:47:53 -0500

    debug

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `OpenMind`,
 	description: `A website for a mental health organization called OpenMind.`,
-	url: `https://openmindinitative.com`, // No trailing slash!
+	url: `https://main--openmindinitative.netlify.app`, // No trailing slash!
 	name: `OpenMind`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, 
