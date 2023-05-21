3f7cd9735e67e8450199ffc5bf948359c678d00d
SShashi <shashi.salavath@gmail.com>
2023-05-20 19:28:11 -0500

    domain

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `OpenMind`,
 	description: `A website for a mental health organization called OpenMind.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	url: `https://openmindinitative.com`, // No trailing slash!
 	name: `OpenMind`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, 
